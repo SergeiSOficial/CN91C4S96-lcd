ce66adad00d854e4833481ee86395a9497da7e5e
SergeiSOficial <geos2@mail.ru>
2021-07-28 11:10:36 +0300

    Added file from prev project

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
-# CN91C4S96-lcd
+# CN91C4S96 7 segment LCD library
+Library for 7-segment lcds based on the CN91C4S96 driver. Very often marked as PDC-6X1.
+
+<img src="extras/photo1.jpg" alt="photo" width="500">
+
+Based on the bitbanging efforts by [anxzhu](https://github.com/anxzhu) (2016-2018).
+APIs rewritten in 2018 to follow the LiquidCrystal format by [valerio\new]
+(https://github.com/5N44P).
+
+Refactored. Removed dependency on any MCU hardware. Added SPI support and text support.
+By Viacheslav Balandin
+https://github.com/hedgehogV/HT1621-lcd
+
+
+## APIs reference
+
+* `CN91C4S96(pPinSet *pCs, pPinSet *pSck, pPinSet *pMosi, pPinSet *pBacklight)`
+Ctor. Starts the lcd with the pin assignment declared. The backlight pin is optional
+
+* `CN91C4S96(pInterface *pSpi, pPinSet *pCs, pPinSet *pBacklight)`
+Starts the lcd with SPI interface. CS and backlight pins are optional. Tested with CPOL=LOW, EDGE=1
+
+* `void clear()`
+Clears the display
+
+* `void backlightOn()`
+Turns on the backlight
+
+* `void backlightOff()`
+Turns off the backlight
+
+* `void batteryLevel(uint8_t percents)`
+Accepts values from 0 to 100. Bigger values will be treated as 100. Battery charge represented by the rectangles above the battery symbol.
+
+* `void print(const char *str)`
+Print string (up to 6 characters)
+Allowed characters: letters, digits, space, minus, underscore. Not allowed characters will be displayed as spaces.
+See characters appearance in `Internal functioning` chapter
+
+* `void print(int32_t num)`
+Prints a signed integer between -99999 and 999999. Larger and smaller values will be displayed as -99999 and 999999
+
+* `void print(float num, int precision)`
+Prints a float with 0 to 3 decimals, based on the `precision` parameter. Default value is 3
+
+* `void print(int32_t multiplied_float, uint32_t multiplier)`
+Prints number with dot. Use it instead float. Float type usage may slow down many systems
+
+* `void displayOff()`
+Turns off the display (doesn't turn off the backlight)
+
+* `void displayOn()`
+Turns the display back on after it has been disabled by `noDisplay()`
+
+## API usage
+
+To start display you have to call one of Ctors with pointers to toggle_Gpio or SpiTx functions.
+You may need a function wrapper. Wrapper example:
+
+```cpp
+// spi wrapper
+void SpiTx(uint8_t *ptr, uint8_t size)
+{
+  HAL_SPI_Transmit(&hspi2, ptr, size, 2000);
+}
+
+// cs wrapper
+void toggle_CS(bool b)
+{
+  HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, b? GPIO_PIN_SET:GPIO_PIN_RESET);
+}
+
+// ctor call example
+CN91C4S96 lcd = CN91C4S96(&SpiTx, &toggle_CS);
+
+// prints "hello" on display
+lcd.print("HELLO");
+```
+
+
+## Internal functioning
+
+Letters example. Source: https://www.dcode.fr/7-segment-display
+Some of them looks a bit strange. But 7-segment display isn't
+the best thing to show letters
+
+<img src="extras/letters.png" alt="photo" width="500">
+
+° -> 0x33 = 10 + 20 + 01 + 02
+C -> 0x1D = 10 + 01 + 04 + 08
+
+```
+  ___10___
+ |        |
+ 01       20
+ |___02___|
+ |        |
+ 04       40
+ |___08___|
+
+```
+
---
+++ src/CN91C4S96.h
@@ -0,0 +1,333 @@
+/*******************************************************************************
+Copyright 2016-2018 anxzhu (github.com/anxzhu)
+Copyright 2018 Valerio Nappi (github.com/5N44P) (changes)
+Based on segment-lcd-with-ht1621 from anxzhu (2016-2018)
+(https://github.com/anxzhu/segment-lcd-with-ht1621)
+
+Partially rewritten and extended by Valerio Nappi (github.com/5N44P) in 2018
+https://github.com/5N44P/ht1621-7-seg
+
+Refactored. Removed dependency on any MCU hardware by Viacheslav Balandin
+https://github.com/hedgehogV/HT1621-lcd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*******************************************************************************/
+
+#ifndef CN91C4S96_H_
+#define CN91C4S96_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    void (*InitI2C)(void);
+    int8_t (*WriteI2C)(const uint8_t*, uint16_t);
+} CN91C4S96_HAL_st;
+
+/**
+     * @brief Construct a new CN91C4S96 object
+     *
+     * Starts the lcd with the pin assignment declared. The backlight pin is optional
+     *
+     * @param hal_ptr
+     */
+void CN91C4S96Init(CN91C4S96_HAL_st *hal_ptr);
+
+/**
+     * @brief Turns on the display (doesn't affect the backlight)
+     */
+void CN91C4S96displayOn();
+
+/**
+     * @brief Turns off the display (doesn't affect the backlight)
+     */
+void CN91C4S96displayOff();
+
+/**
+     * @brief Show battery level.
+     *
+     * @param percents - battery charge state. May vary from 0 up to 100
+     */
+void CN91C4S96batteryLevel(uint8_t percents);
+
+/**
+     * @brief Print string (up to 6 characters)
+     *
+     * @param str String to be displayed.
+     * Allowed: letters, digits, space, minus, underscore
+     * Not allowed symbols will be displayed as spaces. See symbols appearance in README.md
+     */
+void CN91C4S96printStr(const char *str);
+
+/**
+     * @brief Prints a signed integer between -99999 and 999999.
+     * Larger and smaller values will be displayed as -99999 and 999999
+     *
+     * @param num - number to be printed
+     */
+void CN91C4S96printNum(int32_t num);
+
+/**
+     * @brief Prints a float with 0 to 3 decimals, based on the `precision` parameter. Default value is 3
+     * This method may be slow on many systems. Try to avoid float usage.
+     * You may use `void print(int32_t multiplied_float, uint32_t multiplier)` instead
+     *
+     * @param num  - number to be printed
+     * @param precision - precision of the number
+     */
+void CN91C4S96printFloat(float num, uint8_t precision);
+
+/**
+     * @brief Prints number with dot. Use it instead float. Float type usage may slow down many systems
+     */
+void CN91C4S96printFixedPoint(int32_t multiplied_float, uint32_t multiplier);
+
+/**
+     * @brief Clears the display
+     */
+void CN91C4S96clear();
+
+/*!
+    * \brief display min or max value
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    * \param mode true if russian or false if english symbols
+    * \param min true if display min, false if display max
+    */
+void CN91C4S96DispMinMax(bool enable, bool mode, bool min);
+
+/*!
+    * \brief display burst or proriv
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    * \param mode true if russian or false if english symbols
+    */
+void CN91C4S96DispBurst(bool enable, bool mode);
+
+/*!
+    * \brief display leak or TEch`
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    * \param mode true if russian or false if english symbols
+    */
+void CN91C4S96DispLeak(bool enable, bool mode);
+
+/*!
+    * \brief display reverse or obr
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    * \param mode true if russian or false if english symbols
+    */
+void CN91C4S96DispRev(bool enable, bool mode);
+
+/*!
+    * \brief display snowflake frost symbol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispFrost(bool enable);
+
+/*!
+    * \brief display snowflake frost symbol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispQ(bool enable);
+
+/*!
+    * \brief display version or no
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    * \param mode true if russian or false if english symbols
+    */
+void CN91C4S96DispVer(bool enable, bool mode);
+
+/*!
+    * \brief display serial number or B
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    * \param mode true if russian or false if english symbols
+    */
+void CN91C4S96DispSN(bool enable, bool mode);
+
+/*!
+    * \brief display warning sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispWarn(bool enable);
+
+/*!
+    * \brief display magnet sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispMagn(bool enable);
+
+/*!
+    * \brief display left sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispLeft(bool enable);
+
+/*!
+    * \brief display right sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispRight(bool enable);
+
+/*!
+    * \brief display no water sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispNoWater(bool enable);
+/*!
+    * \brief display CRC symbol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispCRC(bool enable);
+/*!
+    * \brief display delta symbol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispDelta(bool enable);
+
+/*!
+    * \brief display T symbol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispT(bool enable);
+
+/*!
+    * \brief display 1 sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96Disp1(bool enable);
+
+/*!
+    * \brief display T2 sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispT2(bool enable);
+
+/*!
+    * \brief display NBFI radio sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispNBFi(bool enable);
+
+/*!
+    * \brief display NB-iot radio sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispNBIoT(bool enable);
+
+/*!
+    * \brief display level of signal
+    *
+    * \param percents if 0 - no display, 0--30 one segment, 30--60 two segments, more than 60 - 3 segments
+    */
+void CN91C4S96SignalLevel(uint8_t percents);
+
+/*!
+    * \brief display DegreePoint sybmol
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    */
+void CN91C4S96DispDegreePoint(bool enable);
+
+/*!
+    * \brief display energy in GJ or Kkal
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    * \param mode true if Kkal or false if GJ symbols
+    * \param perH true for display per hour symbol
+    */
+void CN91C4S96DispEnergyJ(bool enable, bool mode, bool perH);
+
+/*!
+    * \brief display energy in MkWh
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    * \param M if true than display MW else display kW
+    * \param perH if true than display Wh else display W
+    */
+void CN91C4S96DispEnergyW(bool enable, bool M, bool perH);
+
+/*!
+    * \brief display flow in m3 per hour
+    *
+    * \param enable true for enable symbols, false for disable all symbols
+    * \param mode if true than display per chas else display per hour
+    * \param perH if true than display per time else display only volume
+    */
+void CN91C4S96DispFlowM3(bool enable, bool mode, bool perH);
+
+/*!
+     * \brief display flow in GAL per min
+     *
+     * \param enable true for enable symbols, false for disable all symbols
+     * \param perH if true than display per minute else display only volume
+     */
+void CN91C4S96DispFlowGAL(bool enable, bool perH);
+
+/*!
+     * \brief display flow in FT3 per min
+     *
+     * \param enable true for enable symbols, false for disable all symbols
+     * \param perH if true than display per minute else display only volume
+     */
+void CN91C4S96DispFlowFT(bool enable, bool perH);
+
+/*!
+     * \brief display britan meter unit for heat
+     *
+     * \param enable true for enable symbols, false for disable all symbols
+     */
+void CN91C4S96DispMMBTU(bool enable);
+
+/*!
+     * \brief display u.s. gallons or gallons
+     *
+     * \param enable true for enable symbols, false for disable all symbols
+     * \param mode if true then display U.S. else display only GALLONS
+     */
+void CN91C4S96DispGal(bool enable, bool mode);
+
+#define DISPLAY_SIZE 9                             // 16 * 8  = 128 symbols on display plus 2 byte for address
+#define SYS_SIZE 2                                 // 2 byte for address and commands
+#define DATA_SIZE 16                               // 16 * 8  = 128 symbols on display
+#define DISPLAY_BUFFER_SIZE (DATA_SIZE + SYS_SIZE) //  plus 2 byte for address
+
+// defines to set display pin to low or high level
+#define LOW 0
+#define HIGH 1
+
+#endif
